130320c5d4fcb16bf9500d160f51c487188b31ff
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2022-10-01 11:27:58 -0400

    Fix EwwPhysics's errors

--- src/__main__.py
+++ src/__main__.py
@@ -1,22 +1,33 @@
 from . import to_do
-import argparse
 
-# name = input("Enter the name of the task.\n")
-# due_date = input("What day is it due? Format YYYY/MM/DD\n")
-# notes = input("Notes:\n")
+# Make sure the data file exists
+try:
+    with open("list.json"):
+        pass
+except IOError:
+    with open("list.json") as file:
+        file.write("{}")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("action", default="print", help='Must be "delete", "create", or "print"')
-parser.add_argument("name", nargs="?", default="")
-parser.add_argument("date", nargs="?", default="")
-parser.add_argument("notes", nargs="?", default="")
+while True:
+    choice = input("What would you like to do?")
+    print("""1. Create task
+2. Print tasks.
+3. Delete tasks.
+""")
+    try:
+        choice = int(choice)
+    except ValueError:
+        print("That wasn't a valid choice D:")
+        continue
 
-args = parser.parse_args()
-
-if args.action == "delete":
-    to_do.delete_task(args.name)
-elif args.action == "create":
-    to_do.create_task(args.name, args.date, args.notes)
-elif args.action == "print":
-    to_do.print_tasks()
+    if choice == 1:
+        name = input("Enter the name of the task.\n")
+        due_date = input("What day is it due? Format YYYY/MM/DD\n")
+        notes = input("Notes:\n")
+        to_do.create_task(name, due_date, notes)
+    elif choice == 2:
+        to_do.print_tasks()
+    elif choice == 3:
+        name = input("Enter the name of the task to delete.\n")
+        to_do.delete_task(name)
 
--- src/to_do.py
+++ src/to_do.py
@@ -1,39 +1,38 @@
 import json
 
 def create_task(name, due_date, notes):
-    with open("list.json", "r+") as file:
-        try:
-            tasks = json.load(file)
-        except json.decoder.JSONDecodeError:
-            file.seek(0)
-            file.write("{}")
-            tasks = json.load(file)
-        tasks[name] = {"name": name, "due_date": due_date, "notes": notes}
-        file.seek(0)
-        json.dump(tasks, fp=file)
+    with open("list.json") as file:
+        tasks = json.load(file)
+
+    if name in tasks:
+        print("There is already a task with this name.")
+        while True:
+            yn = input("Overwrite? (y/n)\n").lower()
+            if yn == "y":
+                break
+            elif yn == "n":
+                return
+    tasks[name] = {"name": name, "due_date": due_date, "notes": notes}
+    with open("list.json", "w") as file:
+        json.dump(tasks, file)
 
 
 def print_tasks():
     with open("list.json", "r") as file:
         print("".join(item.ljust(20, " ") for item in ("Task", "Due Date", "Notes")))
-        try:
-            tasks = json.load(file)
-            for task in tasks:
-                print("".join(tasks[task][item].ljust(20, " ") for item in tasks[task]))
-        except json.decoder.JSONDecodeError:
-            print("")
+        tasks = json.load(file)
+        for task in tasks.values():
+            print("".join(field.ljust(20, " ") for field in task))
 
 
 def delete_task(name):
-    with open("list.json", "r+") as file:
-        try:
-            tasks = json.load(file)
-            task = tasks.pop(name)
-            if task:
-                print(f"Task {name} was deleted!")
-            else:
-                raise ValueError(f"Task {name} was not found.")
-            file.seek(0)
-            json.dump(tasks, fp=file)
-        except json.decoder.JSONDecodeError:
-            print("Attempted to delete task from empty list.")
+    with open("list.json", "r") as file:
+        tasks = json.load(file)
+        task = tasks.pop(name)
+        if task:
+            print(f"Task {name} was deleted!")
+        else:
+            raise ValueError(f"Task {name} was not found.")
+
+    with open("list.json", "r") as file:
+        json.dump(tasks, file)
